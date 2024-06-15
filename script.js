db9882c7342f155443a351809765c7aa1f1ddf8d
abdulziz12 <abdurashidov200930@gmail.com>
2024-06-15 15:01:38 +0500

    jhkuj
    liuoupl

---
+++ script.js
@@ -0,0 +1,9 @@
+var random = Math.round(Math.random()*100)
+var kopaytirish = random *2
+var bolish = random /2
+alert("sizda hozir random sonlar chiqishni boshlid")
+alert(`Random orqali chiqqan son:${random}`)
+alert("biz random sonni 2 ga ko`paytirdik va bo`ldik")
+alert(`random orqali chiqan son ${random} 
+       biz uni 2 ga kopaytirganimizda ${ kopaytirish}
+       biz uni 2 bolganimizda ${bolish}`)
